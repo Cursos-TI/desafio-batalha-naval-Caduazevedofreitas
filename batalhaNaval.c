6fab07c656ba0cdc29430f5ac48b7313f9b66363
Caduazevedofreitas <barucks123@gmail.com>
2025-08-05 00:24:52 +0000

    Nivel Mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,50 +1,76 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define TAMANHO_TABULEIRO 10
 #define TAMANHO_NAVIO 3
 #define AGUA 0
 #define NAVIO 3
+#define HABILIDADE 5
+#define IMPACTO 5  
 
-int main() {
-    
-    int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO] = {0};
-    int linhaHorizontal =7;
-    int colunaHorizontal =4;
-    int linhaVertical = 2;
-    int colunaVertical =3;
-    int i;
-    int j;
-    
-    
-    
-    printf("       TABULEIRO BATALHA NAVAL!\n");
-    printf("\n");
-    
-    
-    for (int i = 0; i < TAMANHO_NAVIO; i++) {
-        tabuleiro[linhaHorizontal][colunaHorizontal + i] = NAVIO;
+void gerarOctaedro(int matriz[HABILIDADE][HABILIDADE]) {
+    for (int i = 0; i < HABILIDADE; i++) {
+        for (int j = 0; j < HABILIDADE; j++) {
+            if (abs(i - 2) + abs(j - 2) <= 2) matriz[i][j] = 1;
+            else matriz[i][j] = 0;
+        }
     }
+}
 
-    for (int i = 0; i < TAMANHO_NAVIO; i++) {
-        tabuleiro[linhaVertical + i][colunaVertical] = NAVIO;
+void aplicarHabilidade(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO],
+                       int habilidade[HABILIDADE][HABILIDADE],
+                       int origemLinha, int origemColuna) {
+    for (int i = 0; i < HABILIDADE; i++) {
+        for (int j = 0; j < HABILIDADE; j++) {
+            int linhaTabuleiro = origemLinha - HABILIDADE / 2 + i;
+            int colunaTabuleiro = origemColuna - HABILIDADE / 2 + j;
+
+            if (linhaTabuleiro >= 0 && linhaTabuleiro < TAMANHO_TABULEIRO &&
+                colunaTabuleiro >= 0 && colunaTabuleiro < TAMANHO_TABULEIRO) {
+                if (habilidade[i][j] == 1 && tabuleiro[linhaTabuleiro][colunaTabuleiro] == AGUA) {
+                    tabuleiro[linhaTabuleiro][colunaTabuleiro] = IMPACTO;
+                }
+            }
+        }
     }
+}
 
-
-    printf("    ");
-    for (int j = 1; j <= TAMANHO_TABULEIRO; j++) {
-        printf("%2d ", j);
+void exibirTabuleiro(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
+    printf("  ");
+    for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
+        printf("%d ", j);
     }
     printf("\n");
 
     for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
-        printf(" %c  ", 'A' + i);  
-
+        printf("%d ", i);
         for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
-            printf("%2d ", tabuleiro[i][j]);
+            printf("%d ", tabuleiro[i][j]);
         }
         printf("\n");
     }
-   
+}
 
-return 0;
+int main() {
+    int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO] = {0};
+
+    // Posiciona navio vertical (linha 2 a 4, coluna 3)
+    for (int i = 2; i <= 4; i++) {
+        tabuleiro[i][3] = NAVIO;
+    }
+
+    // Posiciona navio isolado (linha 1, coluna 3)
+    tabuleiro[1][3] = NAVIO;
+
+    // Gera matriz de habilidade octaedro
+    int octaedro[HABILIDADE][HABILIDADE];
+    gerarOctaedro(octaedro);
+
+    // Aplica habilidade centrada em (4,5)
+    aplicarHabilidade(tabuleiro, octaedro, 4, 5);
+
+    // Exibe o tabuleiro
+    exibirTabuleiro(tabuleiro);
+
+    return 0;
 }
